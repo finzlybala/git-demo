0c38b2f1ac7691bb789b58dd36efaf4c2a895cc6
finzlybala <finzly_bala@outlook.com>
2022-08-16 15:51:22 +0530

    damru commit

--- Homework/src/com/operations/add/Add1.java
+++ Homework/src/com/operations/add/Add1.java
@@ -5,7 +5,7 @@ import com.operations.variable.daywrk;
 public class Add1 {
 	public int add()
 	 {
-		daywrk d = new daywrk();
+		daywrk d = new daywrk(); // d-daamaru
 		d.setA(5);
 		d.setB(3);
 		return d.getA()+d.getB();
